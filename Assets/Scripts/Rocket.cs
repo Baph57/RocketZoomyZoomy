46b11bedaf2564f886f1b5dae1843db2eb1f1879
Baph <Baph@flavortown.yum>
2019-06-20 14:25:22 -0700

    Lesson 49 commit

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -8,6 +8,7 @@ public class Rocket : MonoBehaviour
     //accessing the rigidbody component on rocket
     //that we added using the unity GUI
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 
     // Start is called before the first frame update
@@ -15,15 +16,17 @@ public class Rocket : MonoBehaviour
     {
         //using GetComponent to assign rigidBody to the actual component
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessUserInput();
+        Thrust();
+        Rotation();
     }
 
-    private void ProcessUserInput()
+    private void Thrust()
     {
         //GetKey() - auto / GetKeyDown()/Up() -for single fire usage
         if (Input.GetKey(KeyCode.Space)) //can trust while rotating
@@ -31,11 +34,25 @@ public class Rocket : MonoBehaviour
             //adding force relative to the object's direction
             //Vector3 struct used to add force upwards
             rigidBody.AddRelativeForce(Vector3.up);
+
+            if (!audioSource.isPlaying) //method to allow no overlapping play
+            {
+                audioSource.Play();
+            }
         }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotation()
+    {
+
+        rigidBody.freezeRotation = true; //taking control of rotation manually
+
         if (Input.GetKey(KeyCode.A))
         {
-            print("A - Left");
-            
             //permanent unity object/component
             //Vector3 allows for adding force forward, still a struct
             transform.Rotate(Vector3.forward);
@@ -43,7 +60,10 @@ public class Rocket : MonoBehaviour
         else if (Input.GetKey(KeyCode.D))
         {
             print("D - Right");
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(-Vector3.forward);
         }
+
+        rigidBody.freezeRotation = false; //resume physics control of rotation
     }
+
 }
